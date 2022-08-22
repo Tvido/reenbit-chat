47baed2d1c10bd1a7cf0138b18a789e13830114b
Volodymyr Viter <mad.tvido@gmail.com>
2022-08-22 15:02:41 +0300

    init project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+// import logo from './logo.svg';
+// <img src={logo} className="App-logo" alt="logo" />
+import './App.css';
+
+function App() {
+  return (
+    <>
+    </>
+  );
+}
+
+export default App;
